fcf78fd054e893563484ebeb9db0638230dc9d49
Nathan Hui <ntlhui@gmail.com>
2013-11-25 22:00:57 -0800

    Removed Arduino pin functions, established that can run Serial operations simultaneously with other interrupts.  Having problems implementing catching loss of RC_PPM.  Look at using overflow interrupt vector to detect.  Cannot use a OCR1A compare interrupt

--- FailSafeBoard/FailSafeBoard.ino
+++ FailSafeBoard/FailSafeBoard.ino
@@ -6,28 +6,32 @@
 /////////////////////////CONFIGURATION/////////////////////////////////////////
 #define PPM_FrLen 22500		// set the PPM frame length (us)
 #define PPM_PulseLen 300	// set the pulse length (us)
-#define S_PPM_PIN 3			// set PPM signal output pin
-#define RC_PPM_PIN 21		// set PPM signal input pin
+#define S_PPM_PIN E5		// set PPM signal output pin
+#define RC_PPM_PIN D0		// set PPM signal input pin
 #define CH_THROTTLE 2		// Set Throttle PPM channel
 #define CH_YAW 0			// Set Yaw PPM channel
 #define CH_PITCH 1			// Set Pitch PPM channel
 #define CH_ROLL 3			// Set Roll PPM channel
 #define CH_AUX 4			// Set Aux PPM channel
+#define ON_TIME_PIN PORTB6	// Set Busy Time Pin
 /////////////////////////CONFIGURATION/////////////////////////////////////////
  
 
 int rc_ppm[8];
 int s_ppm[8];
-int flags = 0;	// bit 0 is Serial Available
+int flags = 0;	// bit 0 is Serial Available, bit 1 is RC PPM Health
+int busyflag = 0;	
 String inputString = "";
 
 void setup(){
 	Serial.begin(9600);
-	pinMode(S_PPM_PIN, OUTPUT);
-	pinMode(RC_PPM_PIN, INPUT);
-	pinMode(13, OUTPUT);
-	pinMode(12, OUTPUT);
-	digitalWrite(13, LOW);
+	
+	DDRE |= (1 << DDE5);	// Set E5 as output
+	DDRD &= ~(1 << DDD0);	// Set D0 as input
+	DDRB |= (1 << DDB7);	// Set B7 as output
+	DDRB |= (1 << DDB6);	// Set B6 as output
+	
+	PORTB &= ~(1 << PORTB7);	// Turn off led (set B7 low)
 	cli();	// disable interrupts
 	TCCR1A = 0;	// Clear timer 1 control register a
 	TCCR1B = 0;	// Clear timer 1 control register b
@@ -37,10 +41,13 @@ void setup(){
 	// Configure timers
 	// Using timer1 for RC_PPM, timer3 for S_PPM
 	OCR3A = 100;	// compare match register, set to dummy value to start
-	TCCR3B |= (1 << WGM12);	// turn on Clear Timer on Compare mode
-	TCCR3B |= (1 << CS11);	// 8 prescaler: 0.5 us @ 16 MHz
-	TIMSK3 |= (1 << OCIE1A);	// emable timer compare interrupt
+	TCCR3B |= (1 << WGM32);	// turn on Clear Timer on Compare mode
+	TCCR3B |= (1 << CS31);	// 8 prescaler: 0.5 us @ 16 MHz
+	TIMSK3 |= (1 << OCIE3A);	// enable timer compare interrupt
 	
+//	OCR1A = 6000;	// compare match register, set to 10000 us
+	TCCR1B |= (1 << WGM12);	// use CTC mode
+	TIMSK1 |= (1 << TOIE1);	// enable timer compare interrupt
 	TCCR1B |= (1 << CS11);	// 8 prescaler: 0.5 us @ 16 MHz
 	EIMSK |= (1 << INT0);	// enable extermal interrupt on pin 2
 	EICRA |= (1 << ISC01);	// Trigger INT0 on falling edge
@@ -54,53 +61,61 @@ void setup(){
 	s_ppm[5] = 900;
 	s_ppm[6] = 900;
 	s_ppm[7] = 900;
+	PORTB |= (1 << ON_TIME_PIN);	// turn on ON_TIME_PIN
 }
 
 void loop(){
-	if(inputString.length())
-		Serial.println(inputString);
-	inputString = "";
-	/* if(flags){
-		PORTB |= (1 << PORTB7);
-	}else{
-		PORTB &= ~(1 << PORTB7);
-	}
-	if(flags & (1 << 1)){
-		char readChar = inputString.charAt(0);
-		switch(readChar){
-			case 97:
-				digitalWrite(12, HIGH);
-				//PORTB |= (1 << PORTB6);
-				break;
-			case 98:
-				digitalWrite(12, LOW);
-				//PORTB &= ~(1 << PORTB6);
-				break;
-			default:
-				digitalWrite(12, LOW);
-				//PORTB &= ~(1 << PORTB6);
-				break;
-		}
-		flags ^= (1 << 1);
-	} */
-	delay(10);
+	// PORTB &= ~(1 << ON_TIME_PIN);
+// /*	while(Serial.available()){
+		// char inChar = (char)Serial.read();
+		// inputString += inChar;
+		// flags |= (1 << 0);
+	// }*/
+// /*	if(flags){
+		// PORTB |= (1 << PORTB7);
+	// }else{
+		// PORTB &= ~(1 << PORTB7);
+	// }*/
+	// if(flags & (1 << 0)){
+		// char readChar = inputString.charAt(0);
+// /*		switch(readChar){
+			// case 97:
+				// PORTB |= (1 << PORTB6);
+				// break;
+			// case 98:
+				// PORTB &= ~(1 << PORTB6);
+				// break;
+			// default:
+				// PORTB &= ~(1 << PORTB6);
+				// break;
+		// }*/
+		// flags ^= (1 << 0);
+		// inputString = "";
+	// }
+// /*	s_ppm[0] = rc_ppm[0];
+	// s_ppm[1] = rc_ppm[1];
+	// s_ppm[2] = rc_ppm[2];
+	// s_ppm[3] = rc_ppm[3];
+	// s_ppm[4] = rc_ppm[4];
+	// s_ppm[5] = rc_ppm[5];
+	// s_ppm[6] = rc_ppm[6];
+	// s_ppm[7] = rc_ppm[7];*/
+	// PORTB |= (1 << ON_TIME_PIN);
+	// delay(100);
 }
 
 void SerialEvent(){
-	while(Serial.available()){
-		char inChar = (char)Serial.read();
-		inputString += inChar;
-		//flags |= (1 << 1);
-	}
 }
 
 ISR(INT0_vect){	// Falling edge interrupt for RC_PPM
+//	PORTB &= ~(1 << ON_TIME_PIN);
 	static unsigned int pulse;
 	static unsigned long counter;
 	static byte channel;
 	
 	counter = TCNT1;	// Get current time
 	TCNT1 = 0;	// Reset timer
+	flags &= ~(1 << 1);	// set good RC PPM Health flag
 	if(counter < 1020){	// must be a pulse if less than 510 us
 		pulse = counter;
 	}else if(counter > 6000){	// sync pulses over 3000us
@@ -109,15 +124,22 @@ ISR(INT0_vect){	// Falling edge interrupt for RC_PPM
 		rc_ppm[channel] = (counter + pulse) / 2;
 		channel++;
 	}
+//	PORTB |= (1 << ON_TIME_PIN);
 }
 
-ISR(TIMER3_COMPA_vect){	
+ISR(TIMER1_OVF_vect){
+	PORTB &= ~(1 << ON_TIME_PIN);
+	flags |= (1 << 1);	// set bad RC PPM Health flag
+	PORTB |= (1 << ON_TIME_PIN);
+}
+
+ISR(TIMER3_COMPA_vect){
+//	PORTB &= ~(1 << ON_TIME_PIN);
 	static boolean state = true;	// set initial state
 	
 	TCNT3 = 0;	// reset timer
 	
 	if(state){	// start pulse
-		// digitalWrite(S_PPM_PIN, LOW);
 		PORTE &= ~(1 << PORTE5);
 		OCR3A = 600;	// timeout at 300 us
 		state = false;
@@ -125,7 +147,6 @@ ISR(TIMER3_COMPA_vect){
 		static byte cur_chan_num;
 		static unsigned int calc_rest;
 		
-		// digitalWrite(S_PPM_PIN, HIGH);
 		PORTE |= (1 << PORTE5);
 		state = true;
 		
@@ -140,4 +161,5 @@ ISR(TIMER3_COMPA_vect){
 			cur_chan_num++;
 		}
 	}
+//	PORTB |= (1 << ON_TIME_PIN);
 }
