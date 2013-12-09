364cf180ed25dd653685af2f41837e31d990a1a1
Nathan Hui <ntlhui@gmail.com>
2013-12-09 00:45:42 -0800

    Made revisions based on feedback from Eric.  Added some housekeeping comments.

--- FailSafeBoard/FailSafeBoard.ino
+++ FailSafeBoard/FailSafeBoard.ino
@@ -1,6 +1,7 @@
 /******************************************************************************
  * This is the code for the Fail Safe Board.
  * Author: Nathan Hui
+ * Date: 12/9/13
  *****************************************************************************/
 
 /////////////////////////CONFIGURATION/////////////////////////////////////////
@@ -16,7 +17,7 @@
 
 int rc_ppm[8];
 int s_ppm[8];
-int flags = 0;	// bit 0 is Serial Available, bit 1 is Bad RC PPM Health
+char flags = 0;	// bit 0 is Serial Available, bit 1 is Bad RC PPM Health
 int busyflag = 0;	
 String inputString = "";
 
@@ -26,9 +27,9 @@ void setup(){
 	// Configure GPIO
 	DDRE |= (1 << DDE5);	// Set E5 as output (S_PPM OUTPUT) (Pin 3)
 	DDRD &= ~(1 << DDD0);	// Set D0 as input (RC_PPM INPUT) (Pin 21)
-	DDRB |= (1 << DDB7);	// Set B7 as output (LED) (Pin 13)
-	DDRB |= (1 << DDB5);	// Set B5 as output (RC_PPM Health) (Pin 11)
-	DDRB |= (1 << DDB4);	// Set B4 as Serial Comm Status (Pin 10)
+	DDRB |= (1 << DDB7) | (1 << DDB5) | (1 << DDB4);
+	// Set B7 as output (LED) (Pin 13), B5 as output (RC_PPM Health) (Pin 11),
+	// B4 as Serial Comm Status (Pin 10)
 	
 	cli();	// disable interrupts
 	
@@ -37,22 +38,18 @@ void setup(){
 	
 	// S_PPM Setup
 	TCCR3A = 0;	// Clear timer 3 control regsiter a
-	TCCR3B = 0;	// Clear timer 3 control register b
 	OCR3A = 100;	// compare match register, set to dummy value to start
-	TCCR3B |= (1 << WGM32);	// turn on Clear Timer on Compare mode
-	TCCR3B |= (1 << CS31);	// 8 prescaler: 0.5 us @ 16 MHz
+	TCCR3B = 0 | (1 << WGM32) | (1 < CS31);
+	// turn on Clear Timer on Compare mode, set 8 prescaler: 0.5 us @ 16 MHz
 	TIMSK3 |= (1 << OCIE3A);	// enable timer compare interrupt
 	
 	// RC_PPM Setup
 	TIMSK1 |= (1 << TOIE1);	// enable overflow interrupt
 	TCCR1A = 0;	// Clear timer 1 control register a
-	TCCR1B = 0;	// Clear timer 1 control register b
-	TCCR1B |= (1 << WGM11);	// use CTC mode
-	TCCR1B |= (1 << CS11);	// 8 prescaler: 0.5 us @ 16 MHz
+	TCCR1B = 0 | (1 << WGM11) | (1 << CS11);
+	// use CTC mode, set 8 prescaler: 0.5 us @ 16 MHz
 	EIMSK |= (1 << INT0);	// enable extermal interrupt on pin 2
 	EICRA |= (1 << ISC01);	// Trigger INT0 on falling edge
-	
-	sei();	// reenable interrupts
 	
 	s_ppm[0] = 900;		// Set default s_ppm values
 	s_ppm[1] = 900;
@@ -62,6 +59,8 @@ void setup(){
 	s_ppm[5] = 900;
 	s_ppm[6] = 900;
 	s_ppm[7] = 900;
+	
+	sei();	// reenable interrupts
 }
 
 void loop(){
@@ -71,9 +70,9 @@ void loop(){
 		flags |= (1 << 0);
 	}
 	if(flags){
-		PORTB |= (1 << ON_TIME_PIN);
+		// PORTB |= (1 << ON_TIME_PIN);
 	}else{
-		PORTB &= ~(1 << ON_TIME_PIN);
+		// PORTB &= ~(1 << ON_TIME_PIN);
 	}
 	if(flags & (1 << 0)){
 		char readChar = inputString.charAt(0);
--- README.md
+++ README.md
@@ -7,4 +7,12 @@ Read in a PPM signal (8 channel), representing RC control signals.  Synthesize a
 
 CURRENT TODO
 ------------
-- Determine if can pass through RC PPM
+
+CHANGELOG
+---------
+12/9/13
+-Add date to header section (FailSafeBoard)
+-Change int flags to char flags (FailSafeBoard)
+-Combined control register writes (FailSafeBoard)
+-Moved sei() to end of setup() (FailSafeBoard)
+-Disabled flag status logic (FailSafeBoard)
